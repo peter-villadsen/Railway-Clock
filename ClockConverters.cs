095cda4321d80de5583f77316692a9500a72168a
Peter Villadsen <petervilladsen@hotmail.com>
2021-02-19 16:58:18 -0800

    1st version

---
+++ ClockConverters.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Railroad.Clock
+{
+    /// <summary>
+    /// Convert milliseconds to a clock angle.
+    /// </summary>
+    [ValueConversion(typeof(DateTime), typeof(double))]
+    public class MilliSecondsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return date.Second * 6 + (date.Millisecond * .006);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Convert seconds to a clock angle.
+    /// </summary>
+    [ValueConversion(typeof(DateTime), typeof(int))]
+    public class SecondsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return date.Second * 6;// (date.Millisecond * .006);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class MinutesAndRatchetToAngleConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)values[0];
+            double retval;
+            if ((bool)values[1])
+                retval = (double)(date.Minute * 6 + (date.Second * 0.1));
+            else
+                retval = (double)(date.Minute * 6);
+
+            return retval;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Convert minutes to a clock angle.
+    /// </summary>
+    [ValueConversion(typeof(DateTime), typeof(int))]
+    public class MinutesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sweepMode = true;
+            DateTime date = (DateTime)value;
+            if (sweepMode)
+                return date.Minute * 6;
+            else
+                return date.Minute * 6 + (date.Second * .1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+
+    /// <summary>
+    /// Convert hours to a clock angle.
+    /// </summary>
+    [ValueConversion(typeof(DateTime), typeof(int))]
+    public class HoursConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime date = (DateTime)value;
+            return (date.Hour * 30) + (date.Minute / 2);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
+//using System.Windows.Input;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+namespace Railroad.Clock
+{
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ICommand MaximizeCommand { get; set; }
+        public static ICommand MinimizeCommand { get; set; }
+        public static ICommand NormalizeCommand { get; set; }
+        public static ICommand ExitCommand { get; set; }
+
+        public bool SecondsSweep
+        {
+            get { return !this.Clock.IsDiscrete; }
+            set { this.Clock.IsDiscrete = !value; }
+        }
+
+        public bool MinutesSweep
+        {
+            get { return this.Clock.IsMinuteSweepMode; }
+            set { this.Clock.IsMinuteSweepMode = value; }
+        }
+
+        public MainWindow()
+        {
+            MaximizeCommand = new RelayCommand((_) =>
+            {
+                this.WindowState = WindowState.Maximized;
+            });
+
+            MinimizeCommand = new RelayCommand((_) =>
+            {
+                this.WindowState = WindowState.Minimized;
+            });
+
+            NormalizeCommand = new RelayCommand((_) =>
+            {
+                this.WindowState = WindowState.Normal;
+            });
+
+            ExitCommand = new RelayCommand((_) =>
+            {
+                App.Current.Shutdown();
+            });
+
+            this.DataContext = this;
+
+            InitializeComponent();
+
+            this.SecondsSweep = true;
+            this.MinutesSweep = false;
+        }
+
+        private void OnLeftMouseButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Clock_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            const int factor = 25;
+            if (e.Delta < 0)
+            {
+                // Decrease size
+                if (this.Width > 100)
+                {
+                    this.Width -= factor;
+                    this.Height -= factor;
+                }
+            }
+            else
+            {
+                // Increase size
+                this.Width += factor;
+                this.Height += factor;
+            }
+        }
+    }
+}
