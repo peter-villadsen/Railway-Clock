3dbed38aaa3e6400ccd63c4a65e41d39cf09c345
Peter Villadsen <petervilladsen@hotmail.com>
2021-03-07 14:02:05 -0800

    Added Arne Jacobsen inspired face. Stored certain properties in the settings, so they persist. Introduced viewmodel.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Data;
-//using System.Windows.Documents;
-//using System.Windows.Input;
-//using System.Windows.Media;
-//using System.Windows.Media.Imaging;
-//using System.Windows.Navigation;
-//using System.Windows.Shapes;
 
 namespace Railroad.Clock
 {
+    using System.ComponentModel;
     using System.Runtime.CompilerServices;
     using System.Windows;
     using System.Windows.Input;
@@ -24,52 +12,14 @@ namespace Railroad.Clock
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static ICommand MaximizeCommand { get; set; }
-        public static ICommand MinimizeCommand { get; set; }
-        public static ICommand NormalizeCommand { get; set; }
-        public static ICommand ExitCommand { get; set; }
-
-        public bool SecondsSweep
-        {
-            get { return !this.Clock.IsDiscrete; }
-            set { this.Clock.IsDiscrete = !value; }
-        }
-
-        public bool MinutesSweep
-        {
-            get { return this.Clock.IsMinuteSweepMode; }
-            set { this.Clock.IsMinuteSweepMode = value; }
-        }
 
         public MainWindow()
         {
-            MaximizeCommand = new RelayCommand((_) =>
-            {
-                this.WindowState = WindowState.Maximized;
-            });
-
-            MinimizeCommand = new RelayCommand((_) =>
-            {
-                this.WindowState = WindowState.Minimized;
-            });
-
-            NormalizeCommand = new RelayCommand((_) =>
-            {
-                this.WindowState = WindowState.Normal;
-            });
-
-            ExitCommand = new RelayCommand((_) =>
-            {
-                App.Current.Shutdown();
-            });
-
-            this.DataContext = this;
+            this.DataContext = new ViewModel.ViewModel(this);
 
             InitializeComponent();
-
-            this.SecondsSweep = true;
-            this.MinutesSweep = false;
         }
+
 
         private void OnLeftMouseButtonDown(object sender, MouseButtonEventArgs e)
         {
---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Railroad.Clock.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public static ICommand MaximizeCommand { get; set; }
+        public static ICommand MinimizeCommand { get; set; }
+        public static ICommand NormalizeCommand { get; set; }
+        public static ICommand ExitCommand { get; set; }
+
+
+        public MainWindow View { get; set; }
+
+        public bool SecondsSweep
+        {
+            get { return Properties.Settings.Default.SweepSeconds; }
+            set
+            {
+                if (value != Properties.Settings.Default.SweepSeconds)
+                {
+                    Properties.Settings.Default.SweepSeconds = value;
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SecondsSweep)));
+                }
+            }
+        }
+
+        public bool MinutesSweep
+        {
+            get { return Properties.Settings.Default.SweepMinutes; }
+            set
+            {
+                if (value != Properties.Settings.Default.SweepMinutes)
+                {
+                    Properties.Settings.Default.SweepMinutes = value;
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MinutesSweep)));
+                }
+            }
+        }
+
+        public ViewModel(MainWindow view)
+        {
+            this.View = view;
+
+            MaximizeCommand = new RelayCommand((_) =>
+            {
+                this.View.WindowState = WindowState.Maximized;
+            });
+
+            MinimizeCommand = new RelayCommand((_) =>
+            {
+                this.View.WindowState = WindowState.Minimized;
+            });
+
+            NormalizeCommand = new RelayCommand((_) =>
+            {
+                this.View.WindowState = WindowState.Normal;
+            });
+
+            ExitCommand = new RelayCommand((_) =>
+            {
+                App.Current.Shutdown();
+            });
+
+            Properties.Settings.Default.PropertyChanged += SettingsChanged;
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Called when any of the properties are changed. We choose to save the 
+        /// values every time, so changes survive crashes and unexpected closedowns.
+        /// </summary>
+        /// <param name="sender">Not used.</param>
+        /// <param name="e">Not used.</param>
+        private void SettingsChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // Save all the user's settings
+            Properties.Settings.Default.Save();
+        }
+
+    }
+}
